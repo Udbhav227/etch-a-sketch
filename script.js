75c0fcf74c1ca998da189327b8d2258896054a01
Udbhav <Udbhav227@gmail.com>
2024-07-17 19:58:46 +0530

    Added modal popup
    When new-grid button is clicked a popup will prompt new number of grid size and on clicking the save button it will render the grid

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
-// <----- REFERENCE ----->
+// <----- REFERENCES ----->
 const pixelContainer = document.querySelector('.pixel-container');
 const resetBtn = document.querySelector('.reset-btn');
 const colorBtn = document.querySelector('.color-btn');
 const classicBtn = document.querySelector('.classic-btn');
 const eraserBtn = document.querySelector('.eraser-btn');
-const dimensionBtn = document.querySelector('.dimension-btn');
+const newGridBtn = document.querySelector('.new-grid-btn');
 
-// <----- PIXEL SETUP ----->
-let pixelCount = 64;
-let pixelWidth = 560 / pixelCount + 'px';
-let pixelHeight = 560 / pixelCount + 'px';
+// <----- MODAL SETUP ----->
+const closeModalButton = document.querySelector('.close-btn');
+const overlay = document.getElementById('overlay');
+const saveButton = document.querySelector('.save-btn');
+
+newGridBtn.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    openModal(modal);
+})
+
+closeModalButton.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    closeModal(modal);
+})
+
+saveButton.addEventListener('click', () => {
+    const gridSize = document.getElementById('grid-size').value;
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+    else if (gridSize && gridSize > 0) {
+        createGrid(gridSize);
+        const modal = document.querySelector('.modal');
+        closeModal(modal);
+    }
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
 
 // <----- MOUSE STUFFS ----->
 let mouseIsDown = false;
 document.addEventListener('mousedown', () => mouseIsDown = true);
 document.addEventListener('mouseup', () => mouseIsDown = false);
 
-// <----- FUNCTIONS ----->
+// <----- PIXEL SETUP ----->
+let defaultPixelCount = 16;
+createGrid(defaultPixelCount);
+
+
+// <----- GRID SETUP ----->
 function createGrid(pixelCount) {
+    pixelContainer.innerHTML = '';
+    let pixelWidth = 560 / pixelCount + 'px';
+    let pixelHeight = 560 / pixelCount + 'px';
     for (let i = 0; i < pixelCount * pixelCount; i++) {
         const pixels = document.createElement('div');
         pixels.classList.add('pixels');
